f95f6cff140dc77b37460be5700f9a13407652d1
Jeffelini <jeffreyliu0117@gmail.com>
2020-09-15 12:36:09 -0400

    Updates

---
+++ precache-manifest.15f144aca5e9ec190336af1010d0f490.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a618c6830f1ea3ed4c3c984492b127ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9546a7e7b04b1ec3f213",
+    "url": "/static/css/main.b2c9ef20.chunk.css"
+  },
+  {
+    "revision": "e4e7c9cf76dc8f6545db",
+    "url": "/static/js/2.91ecc4d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.91ecc4d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9546a7e7b04b1ec3f213",
+    "url": "/static/js/main.7c7fb832.chunk.js"
+  },
+  {
+    "revision": "4e794389d12d3d2bfa24",
+    "url": "/static/js/runtime-main.c5230490.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
